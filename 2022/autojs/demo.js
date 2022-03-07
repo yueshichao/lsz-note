875af30a4c4d85315125d68cc2e022b7eab2d150
leshizhao <leshizhao@gmail.com>
2022-03-07 14:38:35 +0800

    autojs

---
+++ 2022/autojs/demo.js
@@ -0,0 +1,13 @@
+// auto.waitFor();
+"auto";
+toast("start");
+// var btn = text("传感器").findOnce();
+// if (btn) {
+//     btn.click();
+// }
+
+// while(!click("传感器"));
+// Swipe(200, 1000, 210, 400, 500);
+setScreenMetrics(1080, 1920);
+click(100, 150);
+toast("end...");
